6854c990517b0ebe3006ca467e5547ad9ab6a627
Juan Carlo Magat <magatjuancarlo@gmail.com>
2021-03-23 13:03:57 -0700

    Add GeoJSON location to the Park model

--- models/Park.js
+++ models/Park.js
@@ -1,7 +1,41 @@
 const mongoose = require("mongoose");
+const geocoder = require("../utils/geocoder");
 
 const ParkSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+    },
+    coordinates: {
+      type: [Number],
+      index: "2dsphere",
+    },
+    formattedAddress: String,
+  },
+});
+
+// Geocode and create GeoJSON location
+ParkSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  console.log(loc);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+  };
+
+  // Do not save address
+  // this.address = undefined;
+  next();
 });
 
 module.exports = mongoose.model("Park", ParkSchema);
