3d491dd7ec7d678fd2078b3ac4ad44bbc5ca1b81
Juan Carlo Magat <magatjuancarlo@gmail.com>
2021-05-07 14:06:35 -0700

    Install CORS

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv/config");
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/uploads", express.static("uploads"));
 
